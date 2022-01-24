2c6895e27a05e8a286bd71faaf7848215edc5d25
tgiannotta <t.giannotta92@gmail.com>
2022-01-24 18:25:06 +0100

    end snack 2

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,6 +1,7 @@
 <!-- ## Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
-che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
  -->
 
 
@@ -15,6 +16,32 @@ che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocci
 </head>
 <body>
  
-    
+    <?php 
+/*         Vado a prendere i valori che mi sevorno dall'utente */
+        $name = $_GET["name"];
+        $mail = $_GET["email"];
+        $eta = $_GET["age"];   
+    ?>
+    <h1>Check Dati</h1>
+    <div>Il nome che hai inserito è: <?php echo $name; ?>;</div>
+    <div>La mail che hai inserito è: <?php echo $mail; ?>;</div>
+    <div>L'età che hai inserito è: <?php echo $eta; ?>;</div>
+<!--     Contiamo le lettere del nome con un strlen -->
+    <?php
+  
+         $lunghezza_nome = strlen($name);      
+    ?>
+<!--     Stampo l'accesso riuscito o negato -->
+<?php
+if($lunghezza_nome > 3 && strpos($mail , '@') && strpos($mail, '.') && is_numeric($eta)){
+    echo 'tutto ok';
+}
+else{
+    echo 'hai sbalgiato qualcosa';
+}
+?>
+
+
+   
 </body>
 </html>
